13aa1353971493872db89c56591eaa76144462ed
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-08-28 13:38:35 -0300

    feat: create the "config" and use it into "run.sh"
    Changes to be committed:
    modified:   run.sh
    new file:   setup

--- run.sh
+++ run.sh
@@ -16,35 +16,31 @@ mkdir $(pwd)/$1/tests
 cd $(pwd)/$1
 
 echo "" && echo "... Starting \".venv\($1)\""
-
 python -m venv .venv && source ./.venv/bin/activate
 
 echo "" && echo "... Installing the dependencies"
-
 pip install --upgrade pip
 pip install -q $FLAGS
 
 echo "" && echo '... Creating "requirements.txt"'
-
 pip freeze -l > requirements.txt
 
 echo "" && echo '... Creating ".gitignore"'
-
 cat $HOME/$SRC_FOLDER/gitignore > .gitignore
 
 echo "" && echo '... Creating ".env"'
-
 cat $HOME/$SRC_FOLDER/env > .env
 
-echo "" && echo '... Setup "app" and "tests" folders'
+echo "" && echo '... Creating "setup.cfg"'
+cat $HOME/$SRC_FOLDER/setup > setup.cfg
 
+echo "" && echo '... Setup "app" and "tests" folders'
 touch app/__init__.py
 touch app/main.py
 touch app/*/__init__.py
 touch tests/__init__.py
 
 echo "... Configing the git repository" && echo ""
-
 git init && git add . && git checkout -b main && git commit -m "Initial commit" && git checkout -b developer
 
 echo ""
