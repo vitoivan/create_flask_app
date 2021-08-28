095853bb630e22f630350eb1534cf06588b4e5ed
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-08-28 13:07:09 -0300

    refactor: update the "run.sh"

--- run.sh
+++ run.sh
@@ -8,46 +8,44 @@ FLAGS="flask environs ujson pytest flake8"
 SRC_FOLDER=".setup_python_project"
 
 echo "---------- Starting Project ----------"
-
-echo "... Creating Folders ($1)"
+echo "" && echo "... Creating the folders"
 
 mkdir $(pwd)/$1
 mkdir $(pwd)/$1/app
 mkdir $(pwd)/$1/tests
 cd $(pwd)/$1
 
-echo "... Starting \".venv\" ($1)"
+echo "" && echo "... Starting \".venv\($1)\""
 
 python -m venv .venv && source ./.venv/bin/activate
 
-echo "... Installing Dependencies"
+echo "" && echo "... Installing the dependencies"
 
 pip install --upgrade pip
 pip install -q $FLAGS
 
-echo "... Creating requirements.txt"
+echo "" && echo '... Creating "requirements.txt"'
 
 pip freeze -l > requirements.txt
 
-echo "... Creating .gitignore"
+echo "" && echo '... Creating ".gitignore"'
 
 cat $HOME/$SRC_FOLDER/gitignore > .gitignore
 
-echo "... Creating .env"
+echo "" && echo '... Creating ".env"'
 
 cat $HOME/$SRC_FOLDER/env > .env
 
-echo "... Setup app"
+echo "" && echo '... Setup "app" and "tests" folders'
 
 touch app/__init__.py
 touch app/main.py
 touch app/*/__init__.py
 touch tests/__init__.py
 
-#(verificar se o git está instalado)
+echo "... Configing the git repository" && echo ""
 
-#se sim:
 git init && git add . && git checkout -b main && git commit -m "Initial commit" && git checkout -b developer
 
-#se não:
+echo ""
 echo "----------- Setup finished, have a good coding time :) | #happyCoding -----------"
