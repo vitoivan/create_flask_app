ff579b7c5b6e9390bfbfa4a78c6f648e53d9e08c
Marcone Melo Mendonça <marconemendonca@ymail.com>
2021-08-30 16:13:07 -0300

    feat: create the "falke" config file
    Changes to be committed:
    modified:   files/initapp
    modified:   files/run.sh
    renamed:    setup -> files/setup

--- files/run.sh
+++ files/run.sh
@@ -72,10 +72,10 @@ DST=$1
 PWD=$(pwd)
 
 echo "---------- Starting Project in ($MODE) ----------"
-echo "In $PWD/$DST:"
+echo"" && echo "In $PWD/$DST:"
 
 
-echo "... Creating the folders"
+echo"" && echo "... Creating the folders"
 mkdir "$PWD/$DST"
 mkdir "$PWD/$DST/app"
 
@@ -95,8 +95,11 @@ echo"" & echo '... Creating Files'
 pip freeze -l > requirements.txt
 cat $HOME/$SRC_FOLDER/gitignore > .gitignore
 cat $HOME/$SRC_FOLDER/env > .env
+echo "FLASK_APP=app" >> .env.example && echo "FLASK_ENV=" >> .env.example
 cat $HOME/$SRC_FOLDER/initapp > app/__init__.py
+cat $HOME/$SRC_FOLDER/setup > setup.cfg
 touch app/main.py
+touch app/*/__init__.py
 
 if [ "$MODE" == "TEST MODE" ]; then
 	touch tests/__init__.py
@@ -109,4 +112,4 @@ git checkout -q -b main
 git commit -m "Initial commit"
 git checkout -q -b developer
 
-echo"" & echo "----------- Setup finished, have a good coding time :) | #happyCoding -----------"
+echo "" && echo "----------- Setup finished, have a good coding time :) | #happyCoding -----------"
