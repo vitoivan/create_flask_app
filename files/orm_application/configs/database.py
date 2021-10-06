f613fce01eecb2e6f6585380b7ebabc9adff97a8
Andre Flor <andreflor96@hotmail.com>
2021-10-06 11:15:43 -0300

    orm file structure

---
+++ files/orm_application/configs/database.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy import create_engine
+import os
+
+load_dotenv()
+db_uri = os.environ.get("DB_URI")
+engine = create_engine(db_uri)
+if not database_exists(engine.url):
+    create_database(engine.url)
+db = SQLAlchemy()
+
+
+def init_app(app: Flask):
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["JSON_SORT_KEYS"] = False
+
+    app.db = db
+
+    # import models
