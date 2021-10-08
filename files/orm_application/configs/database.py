e35e518728291ff364a6d2ff1972460bf4011e62
Andre Flor <andreflor96@hotmail.com>
2021-10-08 11:14:06 -0300

    fix database init_app

--- files/orm_application/configs/database.py
+++ files/orm_application/configs/database.py
@@ -17,7 +17,8 @@ def init_app(app: Flask):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JSON_SORT_KEYS"] = False
-
+    app.config["JWT_SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    db.init_app(app)
     app.db = db
 
     # import models
